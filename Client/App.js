f6d4cdf0b022f91145f51b94f0d3b2c3d8be4291
tr.youssef <tr.youssef@gmail.com>
2023-05-15 09:57:31 -0600

    making a call to the backend

--- Client/App.js
+++ Client/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { callAPI } from "./src/utils/fetch/callAPI.js";
 
 export default function App() {
+  const [test, setTest] = useState("");
+  callAPI("http://localhost:4001/api/test", "GET", "", "").then((res) => setTest(res.message));
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!!!</Text>
+      <Text>{test}</Text>
       <StatusBar style="auto" />
     </View>
   );
