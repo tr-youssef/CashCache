75de83354b83258c3fb955c6102cd2069ebf48cb
tr.youssef <tr.youssef@gmail.com>
2023-05-15 11:00:57 -0600

    make react navigation

--- Client/App.js
+++ Client/App.js
@@ -1,17 +1,35 @@
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { callAPI } from "./src/utils/fetch/callAPI.js";
+import { useCallback } from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import { navigationRef } from "./src/utils/navigation/RootNavigation.js";
+import Welcome from "./src/views/Welcome.js";
+import Page1 from "./src/views/Page1.js";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [test, setTest] = useState("");
-  callAPI("http://localhost:4001/api/test", "GET", "", "").then((res) => setTest(res.message));
+  const Stack = createNativeStackNavigator();
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
-      <Text>{test}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer onReady={onLayoutRootView} ref={navigationRef}>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+        <Stack.Screen name="Page1" component={Page1} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
