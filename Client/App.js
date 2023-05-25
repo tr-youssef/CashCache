25a83f493aa2d7c6ae5656a2c173f4f5f3090ad4
tr.youssef <tr.youssef@gmail.com>
2023-05-25 00:41:09 -0600

    refactoring APP.js

--- Client/App.js
+++ Client/App.js
@@ -1,15 +1,9 @@
 import { useCallback } from "react";
 import { StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import { Icon } from "@rneui/themed";
-import { navigationRef } from "./src/utils/navigation/RootNavigation.js";
-import DashboardNavigator from "./src/views/Dashboard/DashboardNavigator.js";
-import CategoriesNavigator from "./src/views/Categories/CategoriesNavigator.js";
-import AccountsNavigator from "./src/views/Accounts/AccountsNavigator.js";
-import TransactionsNavigator from "./src/views/Transactions/TransactionsNavigator.js";
+import BottomMenu from "./src/components/BottomMenu/BottomMenu.js";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,48 +20,7 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
-  return (
-    <NavigationContainer onReady={onLayoutRootView} ref={navigationRef}>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="DashboardNavigator"
-          component={DashboardNavigator}
-          options={{
-            tabBarLabel: "Dashboard",
-            tabBarIcon: () => <Icon name="dashboard" type="MaterialIcons" />,
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="CategoriesNavigator"
-          component={CategoriesNavigator}
-          options={{
-            tabBarLabel: "Categories",
-            tabBarIcon: () => <Icon name="category" type="MaterialIcons" />,
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="AccountsNavigator"
-          component={AccountsNavigator}
-          options={{
-            tabBarLabel: "Accounts",
-            tabBarIcon: ({ color, size }) => <Icon name="account-balance-wallet" type="MaterialIcons" />,
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="TransactionsNavigator"
-          component={TransactionsNavigator}
-          options={{
-            tabBarLabel: "Transactions",
-            tabBarIcon: ({ color, size }) => <Icon name="format-list-bulleted" type="MaterialIcons" />,
-            headerShown: false,
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  return <BottomMenu />;
 }
 
 const styles = StyleSheet.create({
---
+++ Client/src/components/BottomMenu/BottomMenu.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Icon } from "@rneui/themed";
+import { navigationRef } from "../../../src/utils/navigation/RootNavigation.js";
+import { NavigationContainer } from "@react-navigation/native";
+import DashboardNavigator from "../../../src/views/Dashboard/DashboardNavigator.js";
+import CategoriesNavigator from "../../../src/views/Categories/CategoriesNavigator.js";
+import AccountsNavigator from "../../../src/views/Accounts/AccountsNavigator.js";
+import TransactionsNavigator from "../../../src/views/Transactions/TransactionsNavigator.js";
+
+const BottomMenu = () => {
+  return (
+    <NavigationContainer onReady={onLayoutRootView} ref={navigationRef}>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="DashboardNavigator"
+          component={DashboardNavigator}
+          options={{
+            tabBarLabel: "Dashboard",
+            tabBarIcon: () => <Icon name="dashboard" type="MaterialIcons" />,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="CategoriesNavigator"
+          component={CategoriesNavigator}
+          options={{
+            tabBarLabel: "Categories",
+            tabBarIcon: () => <Icon name="category" type="MaterialIcons" />,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="AccountsNavigator"
+          component={AccountsNavigator}
+          options={{
+            tabBarLabel: "Accounts",
+            tabBarIcon: ({ color, size }) => <Icon name="account-balance-wallet" type="MaterialIcons" />,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="TransactionsNavigator"
+          component={TransactionsNavigator}
+          options={{
+            tabBarLabel: "Transactions",
+            tabBarIcon: ({ color, size }) => <Icon name="format-list-bulleted" type="MaterialIcons" />,
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default BottomMenu;
