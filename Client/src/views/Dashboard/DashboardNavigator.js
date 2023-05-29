ad50b9c6ee83afc252d8356f150732eff186c898
tr.youssef <tr.youssef@gmail.com>
2023-05-29 10:09:06 -0600

    adding dark & light theme

--- Client/src/views/Dashboard/DashboardNavigator.js
+++ Client/src/views/Dashboard/DashboardNavigator.js
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { DrawerContext } from "../../utils/context/DrawerContext.js";
 import { Icon } from "@rneui/themed";
 import Dashboard from "./Dashboard.js";
 import Settings from "../Settings/Settings.js";
+import { colors } from "../../utils/theme/theme.js";
 
 const DashboardNavigator = ({ navigation }) => {
   const Stack = createNativeStackNavigator();
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  //const theme = useColorScheme();
+  const MyLightTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      backgroundColor: colors.light.lightBlue,
+    },
+  };
+  const MyDarkTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: "red",
+    },
+  };
   return (
     <DrawerContext.Provider value={{ drawerIsOpen, setDrawerIsOpen }}>
       <Stack.Navigator>
