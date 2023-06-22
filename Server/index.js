6aa15c2a213fa020cc52b8d50872040ea7626147
Zack Spring Chief <jzacksc@gmail.com>
2023-06-22 15:34:04 -0600

    added ai api to index.js

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,9 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 dotenv.config();
 
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
+
 app.use("/api/test", test);
 app.use("/api/users", routeUsers);
 app.use("/api/categories", routeCategories);
@@ -24,5 +27,26 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
   .catch((error) => console.log(error));
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.CHAT_API_KEY,
+  })
+);
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+userInterface.prompt();
+userInterface.on("line", async (input) => {
+  const res = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  });
+  console.log(res.data.choices[0].message.content);
+});
